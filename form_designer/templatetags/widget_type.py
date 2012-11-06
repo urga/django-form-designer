8aa98415417f8cedbbe756302acf4cbbd5805be1
Venelin Stoykov <venelin@magicbg.com>
2012-11-06 10:35:44 +0200

    Improve widget_type filters
    - Change de behaviour of field_type filter - must accept a BoundField and return the type of his actual field
    - Add widget_type filter - accept BoundField and return the type of the widget that will render this field

--- form_designer/templatetags/widget_type.py
+++ form_designer/templatetags/widget_type.py
@@ -1,6 +1,18 @@
 from django import template
 register = template.Library()
 
-@register.filter('field_type')
-def field_type(obj):
-    return obj.__class__.__name__
+
+@register.filter
+def field_type(field):
+    """
+    Show actual field type of a BoundField
+    """
+    return field.field.__class__.__name__
+
+
+@register.filter
+def widget_type(field):
+    """
+    Show widget type of the BoundField
+    """
+    return field.field.widget.__class__.__name__
